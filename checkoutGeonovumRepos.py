2bd21d9d982a33f53191936a13eff69c9ac8c0b5
Wilko Quak <w.quak@geonovum.nl>
2023-04-28 14:28:50 +0200

    update

--- checkoutGeonovumRepos.py
+++ checkoutGeonovumRepos.py
@@ -7,28 +7,30 @@ import json
 import os
 from git import Repo
 
-output = subprocess.check_output('gh repo list Geonovum -L 3 --json name')
+output = subprocess.check_output('gh repo list Geonovum -L 400 --json name,isEmpty')
 data = json.loads(output)
 
-data = json.loads('''
-[
-  {
-    "name": "dso-cimop"
-  },
-  {
-    "name": "DashboardGit"
-  }
-]
-''')
+#data = json.loads('''
+#[
+  #{
+    #"name": "dso-cimop"
+  #},
+  #{
+    #"name": "DashboardGit"
+  #}
+#]
+#''')
 
 for x in data:
     repo = x['name']
-    if os.path.isdir(repo):
+    isEmpty = x['isEmpty']
+    
+    if isEmpty:
+        print('manually skipping repo {} because empty.'.format(repo))
+    elif os.path.isdir(repo):
         print('repo {} exisits updating'.format(repo))
         repo = Repo(os.path.abspath(repo));
         repo.git.pull()
-
     else:
         print('repo {} does not exist checking out'.format(repo))
-        co = subprocess.check_output('gh repo clone Geonovum/{}'.format(repo))
-        print('co={}'.format(co))
+        subprocess.check_output('gh repo clone Geonovum/{}'.format(repo))
